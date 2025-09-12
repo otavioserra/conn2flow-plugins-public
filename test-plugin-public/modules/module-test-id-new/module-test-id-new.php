d9efcda311e278843341e218fc13b6d1e49c22b2
Otávio Campos de Abreu Serra <otavioserra@gmail.com>
2025-09-12 14:57:48 -0300

    [test-plugin-public][Test Plugin Public] added: novo módulo de tests (v0.1.3)

--- test-plugin-public/modules/module-test-id-new/module-test-id-new.php
+++ test-plugin-public/modules/module-test-id-new/module-test-id-new.php
@@ -2,7 +2,7 @@
 
 global $_GESTOR;
 
-$_GESTOR['modulo-id']							=	'module-test-id';
+$_GESTOR['modulo-id']							=	'module-test-id-new';
 $_GESTOR['modulo#'.$_GESTOR['modulo-id']] = json_decode(file_get_contents(__DIR__ . '/'.$_GESTOR['modulo-id'].'.json'), true);
 
 // Example plugin module
